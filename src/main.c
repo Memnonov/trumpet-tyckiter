88f403ad1fdf67b0019f56121fc2b70eb45b3dce
mikko <esi@merkki.fi>
2024-02-13 22:32:17 +0200

    added doot doots (not really refactoring...)

--- src/main.c
+++ src/main.c
@@ -15,14 +15,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Struct for keeping track of initialized modules, windows etc.
-typedef struct {
-  int SDL_Init;
-  int MIX_Init;
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-} GAME;
-
 // Struct for the trumpet: graphics and keys pressed etc.
 // Also the SDL_Rects for the key rectangles.
 typedef struct {
@@ -36,8 +28,22 @@ typedef struct {
   SDL_Rect key_3_rect;
 } GAME_Trumpet;
 
-void GAME_QuitAll(void);                            // automagic cleanup
-void GAME_PlaceTrumpetKeys(GAME_Trumpet *trumpet);  // Init the valve rectangles
+typedef struct {
+  int place_holder;
+} GAME_Audio;
+
+// Struct for keeping track of initialized modules, windows etc.
+typedef struct {
+  int SDL_Init;
+  int MIX_Init;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+} GAME;
+
+// Automagic cleanup
+void GAME_QuitAll(GAME *game, GAME_Trumpet *trumpet, GAME_Audio *audio);
+// Put the keys in their place
+void GAME_PlaceTrumpetKeys(GAME_Trumpet *trumpet);
 
 // Main function!
 int main(void) {
@@ -52,10 +58,11 @@ int main(void) {
   // täähän pyrii nyt vaan, jos pwd on juuri...
   const char *VALVES_PATH = "resources/trumpet_valves.png";
   const char *KEY_PATH = "resources/trumpet_key.png";
-  const char *SOUND_FANFARE = "resources/trumpet_fanfare.wav";
+  const char *SOUND_F4 = "resources/sound/F4.mp3";
 
   // Initialize GAME.
-  GAME game = {.SDL_Init = 0, .MIX_Init = 0, .window = NULL, .renderer = NULL};
+  GAME game = {
+      .SDL_Init = -1, .MIX_Init = -1, .window = NULL, .renderer = NULL};
 
   // Initialize THE HORN!
   GAME_Trumpet trumpet = {.valves_text = NULL,
@@ -63,6 +70,11 @@ int main(void) {
                           .key_1_pressed = 0,
                           .key_2_pressed = 0,
                           .key_3_pressed = 0};
+
+  // Initialize GAME_Audio.
+  GAME_Audio audio = {
+    .place_holder = 666  // This is sick, but cool...
+  };
 
   // Initialize SDL.
   game.SDL_Init = SDL_Init(INIT_FLAGS);
@@ -78,9 +90,10 @@ int main(void) {
 
   // Open the default audio device. Initializes the mixer too(?)
   game.MIX_Init = Mix_OpenAudio(48000, AUDIO_S16SYS, 2, 2048);
-      // 0 if OK
-      if (game.MIX_Init) {
+  // 0 if OK
+  if (game.MIX_Init) {
     printf("\nCouldn't open audio device: %s", SDL_GetError());
+    // TODO: Have these checks use the GAME_QuitAll() function!
     SDL_Quit();
     return EXIT_FAILURE;
   }
@@ -109,8 +122,7 @@ int main(void) {
   // Create the trumpet valves and keys textures.
   // SDL_Texture *trumpet_valves_texture = NULL;  // Now in the GAME_Trumpet
   // struct SDL_Texture *trumpet_key_texture = NULL;     // These too
-  trumpet.valves_text = IMG_LoadTexture(
-      game.renderer, VALVES_PATH);
+  trumpet.valves_text = IMG_LoadTexture(game.renderer, VALVES_PATH);
   trumpet.keys_text = IMG_LoadTexture(game.renderer, KEY_PATH);
   if (trumpet.valves_text == NULL) {
     printf("Couldn't load trumpet valves!");
@@ -131,7 +143,7 @@ int main(void) {
   GAME_PlaceTrumpetKeys(&trumpet);
 
   // Load a sound effect.
-  Mix_Chunk *sound_fanfare = Mix_LoadWAV(SOUND_FANFARE);
+  Mix_Chunk *sound_fanfare = Mix_LoadWAV(SOUND_F4);
   if (sound_fanfare == NULL) {
     printf("Couldn't load fanfare : (  (%s))", SDL_GetError());
     SDL_DestroyWindow(game.window);
@@ -248,17 +260,8 @@ int main(void) {
     SDL_Delay(1000 / 60);
   }
 
-  // // Wait to see the results.
-  // SDL_Delay(3000);
-
-  // Close all
-  SDL_DestroyWindow(game.window);
-  SDL_DestroyRenderer(game.renderer);
-  SDL_DestroyTexture(trumpet.valves_text);
-  SDL_DestroyTexture(trumpet.keys_text);
   Mix_FreeChunk(sound_fanfare);
-  SDL_CloseAudio();
-  SDL_Quit();
+  GAME_QuitAll(&game, &trumpet, &audio);
   printf("Program exited succesfully!");
 
   return EXIT_SUCCESS;
@@ -267,20 +270,54 @@ int main(void) {
 /* void GAME_QuitAll(void) (might need arguments eventually!)
  *
  * Conveniently closes all initiated SDL2 modules. */
-void GAME_QuitAll(void) {
-  // SDL_init       returns 0 if OK
+void GAME_QuitAll(GAME *game, GAME_Trumpet *trumpet, GAME_Audio *audio) {
+  // SDL_init       returns 0 if OK (SDL_Quit() deals with this?)
   // Mix_OpenAudio  returns 0 if OK
-  //
-  // GAME_Trumpet has pointers to valves_text and keys_text
-  // Sounds need to be destroyed.
+  printf("\nClosing audio");
+  if (game->MIX_Init == 0) {
+    printf("\n  Audio closed!");
+    SDL_CloseAudio();
+  }
 
-  SDL_Quit();
+  // Destroy window and renderer
+  printf("\nDestroying Window");
+  if (game->window != NULL) {
+    SDL_DestroyWindow(game->window);
+    printf("\n  Window destroyed!");
+  }
+  printf("\nDestroying Renderer");
+  if (game->renderer != NULL) {
+    SDL_DestroyRenderer(game->renderer);
+    printf("\n  Renderer destroyed!");
+  }
+
+  // GAME_Trumpet has pointers to valves_text and keys_text
+  printf("\nDestroying keys");
+  if (trumpet->keys_text != NULL) {
+    SDL_DestroyTexture(trumpet->keys_text);
+    printf("\n  Keys destroyed!");
+  }
+  printf("\nDestroying valves");
+  if (trumpet->valves_text != NULL) {
+    SDL_DestroyTexture(trumpet->valves_text);
+    printf("\n  Valves destroyed!");
+  }
+
+  // TODO: Destroy sound!
+
+  if (game->MIX_Init == 0) {
+    Mix_Quit();
+    printf("\n  Mix Quit!");
+  }
+  if (game->SDL_Init == 0) {
+    SDL_Quit();
+    printf("\n  SDL Quit!");
+  }
 }
 
 /* void GAME_PlaceTrumpetKeys(GAME_Trumpet *trumpet)
  *
  * Initializes the rectangles for the key locations. */
-
 void GAME_PlaceTrumpetKeys(GAME_Trumpet *trumpet) {
   const unsigned int key_gap = 95;
   SDL_QueryTexture(trumpet->keys_text, NULL, NULL, &trumpet->key_1_rect.w,
@@ -290,14 +327,12 @@ void GAME_PlaceTrumpetKeys(GAME_Trumpet *trumpet) {
   SDL_QueryTexture(trumpet->keys_text, NULL, NULL, &trumpet->key_3_rect.w,
                    &trumpet->key_3_rect.h);
   // Spread the keys.
+  trumpet->key_1_rect.y = trumpet->key_2_rect.y = trumpet->key_3_rect.y = 150;
   trumpet->key_1_rect.x = 170;
-  trumpet->key_1_rect.y = 150;
   SDL_QueryTexture(trumpet->keys_text, NULL, NULL, &trumpet->key_1_rect.w,
                    &trumpet->key_1_rect.h);
   trumpet->key_2_rect.x = trumpet->key_1_rect.x + key_gap;
-  trumpet->key_2_rect.y = 150;
   SDL_QueryTexture(trumpet->keys_text, NULL, NULL, &trumpet->key_1_rect.w,
                    &trumpet->key_1_rect.h);
   trumpet->key_3_rect.x = trumpet->key_2_rect.x + key_gap;
-  trumpet->key_3_rect.y = 150;
 }
